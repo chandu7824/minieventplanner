c29ba6daee557c81a469f40833498a632be43d0e
Chandu M <chandum7824@gmail.com>
2025-12-18 05:30:27 +0530

    solved cors error

--- server/server.js
+++ server/server.js
@@ -57,12 +57,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
+const allowedOrigins = [
+  "https://minieventplanner.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
